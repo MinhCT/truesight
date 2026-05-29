583fca04cac9a5352ca481780273840f5714ba26
Niles <minhct1997@gmail.com>
2026-05-29 16:42:53 +0700

    Intialize custom Singer tap

---
+++ tap-youtube-persona/tap_youtube_persona/__main__.py
@@ -0,0 +1,7 @@
+"""YoutubePersona entry point."""
+
+from __future__ import annotations
+
+from tap_youtube_persona.tap import TapYoutubePersona
+
+TapYoutubePersona.cli()
---
+++ tap-youtube-persona/tap_youtube_persona/client.py
@@ -0,0 +1,148 @@
+"""REST client handling, including YoutubePersonaStream base class."""
+
+from __future__ import annotations
+
+import decimal
+import sys
+from typing import TYPE_CHECKING, Any, ClassVar
+
+from singer_sdk import SchemaDirectory, StreamSchema
+from singer_sdk.authenticators import BearerTokenAuthenticator
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.pagination import BaseAPIPaginator  # noqa: TC002
+from singer_sdk.streams import RESTStream
+
+from tap_youtube_persona import schemas
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+    import requests
+    from singer_sdk.helpers.types import Context
+    from singer_sdk.streams.rest import HTTPRequest, PageContext
+
+
+# TODO: Delete this is if not using json files for schema definition
+SCHEMAS_DIR = SchemaDirectory(schemas)
+
+
+class YoutubePersonaStream(RESTStream):
+    """YoutubePersona stream class."""
+
+    # Update this value if necessary or override `parse_response`.
+    records_jsonpath = "$[*]"
+
+    # Update this value if necessary or override `get_new_paginator`.
+    next_page_token_jsonpath = "$.next_page"  # noqa: S105
+
+    schema: ClassVar[StreamSchema] = StreamSchema(SCHEMAS_DIR)
+
+    @override
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        # TODO: hardcode a value here, or retrieve it from self.config
+        return "https://api.mysample.com"
+
+    @override
+    @property
+    def authenticator(self) -> BearerTokenAuthenticator:
+        """Return a new authenticator object.
+
+        Returns:
+            An authenticator instance.
+        """
+        return BearerTokenAuthenticator(token=self.config["auth_token"])
+
+    @property
+    @override
+    def http_headers(self) -> dict:
+        """Return the http headers needed.
+
+        Returns:
+            A dictionary of HTTP headers.
+        """
+        # If not using an authenticator, you may also provide inline auth headers:
+        # headers["Private-Token"] = self.config.get("auth_token")
+        return {}
+
+    @override
+    def get_new_paginator(self) -> BaseAPIPaginator | None:
+        """Create a new pagination helper instance.
+
+        If the source API can make use of the `next_page_token_jsonpath`
+        attribute, or it contains a `X-Next-Page` header in the response
+        then you can remove this method.
+
+        If you need custom pagination that uses page numbers, "next" links, or
+        other approaches, please read the guide: https://sdk.meltano.com/en/v0.25.0/guides/pagination-classes.html.
+
+        Returns:
+            A pagination helper instance, or ``None`` to indicate pagination
+            is not supported.
+        """
+        return super().get_new_paginator()
+
+    @override
+    def get_http_request(self, *, page: PageContext[Any]) -> HTTPRequest:
+        """Return a request object for this stream.
+
+        Args:
+            page: An object containing the stream partition or context dictionary,
+                and the next page token if applicable.
+
+        Returns:
+            An HTTP request for this stream.
+        """
+        request = super().get_http_request(page=page)
+        request.params["page"] = page.next_page_token
+
+        if self.replication_key:
+            request.params["sort"] = "asc"
+            request.params["order_by"] = self.replication_key
+
+        # Optionally, add the payload for a POST request
+        # request.data = ...
+        return request
+
+    @override
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
+        # TODO: Parse response body and return a set of records.
+        yield from extract_jsonpath(
+            self.records_jsonpath,
+            input=response.json(parse_float=decimal.Decimal),
+        )
+
+    @override
+    def post_process(
+        self,
+        row: dict,
+        context: Context | None = None,
+    ) -> dict | None:
+        """As needed, append or transform raw data to match expected structure.
+
+        Note: As of SDK v0.47.0, this method is automatically executed for all stream types.
+        You should not need to call this method directly in custom `get_records` implementations.
+
+        Args:
+            row: An individual record from the stream.
+            context: The stream context.
+
+        Returns:
+            The updated record dictionary, or ``None`` to skip the record.
+        """
+        # TODO: Delete this method if not needed.
+        return row
